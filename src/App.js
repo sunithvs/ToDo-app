a16519e531d0074d4778c0d471801c45274574d4
sunith vs <sunithvazhenkada@gmail.com>
2021-05-31 17:43:22 +0530

    completed basic Design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1 className ='heading' >Todo Website</h1>
+      <br/>
+      <div className='row'>
+      <div className='main-buttons'> Pending </div>
+        
+         <div className='main-buttons'> Add </div>
+         <div className='main-buttons' > Completed </div>       
+
+      </div>
     </div>
   );
 }

1b354c4efdbb565b6748348219c8117a8328fb13
sunith vs <sunithvazhenkada@gmail.com>
2021-06-02 13:02:06 +0530

    Commit before creating separate folder

--- src/App.js
+++ src/App.js
@@ -17,21 +17,26 @@ function App() {
         
       
       <div className='container text-white'>
-      <button  type="button" className="btn left dropdown-toggle" ></button>
-      
-      <input className='text-box' type='text' onChange={
-          (e)=> {setText(e.target.value)}
-      } ></input>   
-      <button  type="button" className="btn" onClick={
-          ()=> {setTodo([...toDo,text])}
-      } > Add Task </button>
+          <button  type="button" className="btn left dropdown-toggle" ></button>
+          
+          <input className='text-box' type='text' onChange={
+              (e)=> {setText(e.target.value)}
+          } /> 
+          <button  type="button" className="btn" onClick={
+              ()=> { if(text){setTodo([...toDo,{id:Date.now(),content:text,status:false}]);console.log(toDo)}}
+          } > Add Task </button>
 
       
       </div> 
-       <div className='col-12 col-lg-6 content' >
-
-     
-       </div>
+          
+      <div className='container text-white'>
+          {
+            toDo.map((obj)=> obj.content ? <div className="toDo"><h3><input className='check-box' type='checkbox' onChange={
+              (e)=> obj.status = e.target.checked
+            } ></input>{obj.content}<i className="icon-right fas fa-times-circle"></i></h3></div>:null)
+          }
+         
+      </div>
     </div>
   );
 }
